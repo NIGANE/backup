7e5f3c77dd13044a254d3a15ca571d1e7d51f0ff
NIGANE <merkhtachraf163@gmail.com>
2025-07-23 00:50:49 +0100

    ok

--- days/c04/ex04/ft_putnbr_base.c
+++ days/c04/ex04/ft_putnbr_base.c
@@ -12,20 +12,66 @@
 
 #include <unistd.h>
 
+void	ft_putchar(char a)
+{
+	write(1, &a, 1);
+}
+
+int	ft_strlen(char	*str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
 int	is_valid(char	*base)
 {
+	int	i;
+	int	j;
+
 	if (ft_strlen(base) <= 1)
 		return (0);
+	i = 0;
+	while (base[i] != '\0')
+	{
+		if (base[i] == '-' || base[i] == '+')
+			return (0);
+		j = i + 1;
+		while (base[j] != '\0')
+		{
+			if (base[i] == base[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
 
+void	print_base(int nb, char *base)
+{
+	int	base_len;
+
+	base_len = ft_strlen(base);
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb *= -1;
+	}
+	if (nb > base_len)
+	{
+		print_base(nb / base_len, base);
+	}
+	ft_putchar(base[nb % base_len]);
 }
 
 void	ft_putnbr_base(int nbr, char *base)
 {
-	int	i;
-	int	base_len;
-
 	if (is_valid(base))
 	{
+		print_base(nbr, base);
 	}
-
 }
