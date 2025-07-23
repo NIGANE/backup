d63da56ea2fa2fe907d49d5e0406108415bbf663
Achraf Merkht <amerkht@e2r10p12.1337.ma>
2025-07-23 16:44:33 +0100

    ok

--- days/c03/ex04/ft_strstr.c
+++ days/c03/ex04/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: amerkht <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/21 18:44:52 by amerkht           #+#    #+#             */
-/*   Updated: 2025/07/21 18:54:49 by amerkht          ###   ########.fr       */
+/*   Updated: 2025/07/23 16:43:09 by amerkht          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,9 @@ char	*ft_strstr(char *str, char *to_find)
 	int	j;
 
 	i = 0;
+	j = 0;
+	if (to_find[i] == '\0')
+		return (str);
 	while (str[i] != '\0')
 	{
 		j = 0;
