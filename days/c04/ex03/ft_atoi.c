2d1323b36a14d717f45982b35d4b9bfb7c45f9b3
NIGANE <merkhtachraf163@gmail.com>
2025-07-26 00:28:53 +0100

    NIGANE

--- days/c04/ex03/ft_atoi.c
+++ days/c04/ex03/ft_atoi.c
@@ -1,65 +1,24 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: amerkht <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/22 15:05:38 by amerkht           #+#    #+#             */
-/*   Updated: 2025/07/24 14:58:58 by amerkht          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
-int	is_digit(char a)
-{
-	if (a >= '0' && a <= '9')
-		return (1);
-	return (0);
-}
 
-int	is_valid(char *str)
+int	ft_atoi(char *str)
 {
+	int	re;
+	int	sign;
 	int	i;
-	int	start;
 
 	i = 0;
-	start = 0;
-	while (str[i] != '\0')
+	re = 0;
+	sign = 1;
+	while((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
+		i++;
+	while (str[i] == '+' || str[i] == '-')
 	{
-		if (str[i] == '-' || str[i] == '+')
-			start = 1;
-		if((str[i] == ' ' || (str[i] >= 9 && str[i] <= 13)) && start == 1)
-			return (0);
-		if(is_digit(str[i]))
-			return (1);
+		if (str[i] == '-')
+			sign *= -1;
 		i++;
 	}
-	return (1);
-}
+	while (str[i] >= '0' && str[i] <= '9')
+		re = re*10 + str[i++] - '0';
 
-int	ft_atoi(char	*str)
-{
-	int	i;
-	int	sign;
-	int	re;
-
-	i = 0;
-	sign = 1;
-	re = 0;
-	if(is_valid(str))
-	{
-		while (str[i] != '\0')
-		{
-			if (str[i] == '-')
-				sign *= -1;
-			else if (str[i] >= '0' && str[i] <= '9')
-			{
-				while (str[i] >= '0' && str[i] <= '9')
-					re = (re * 10) + (str[i++] - '0');
-				return (re * sign);
-			}
-			i++;
-		}
-	}
-	return (0);
+	return re*sign;
 }
--- days/c04/ex05/ft_atoi_base.c
+++ days/c04/ex05/ft_atoi_base.c
@@ -1,120 +1,122 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi_base.c                                     :+:      :+:    :+:   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: amerkht <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/23 07:32:23 by amerkht           #+#    #+#             */
-/*   Updated: 2025/07/24 16:14:31 by amerkht          ###   ########.fr       */
+/*   Created: 2025/07/22 21:25:05 by amerkht           #+#    #+#             */
+/*   Updated: 2025/07/24 15:22:20 by amerkht          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	is_valid(char	*base)
-{
-	int	i;
-	int	j;
+#include <unistd.h>
 
-	i = 0;
-	while (base[i])
-	{
-		if (base[i] == '+' || base[i] == '-')
-			return (0);
-		if(base[i] == ' ' || (base >= 9 && base <= 13))
-			return (0);
-		j = i + 1;
-		while (base[j] != '\0')
-		{
-			if (base[i] == base[j])
-				return (0);
-			j++;
-		}
-		i++;
-	}
-	if (i <= 1)
-		return (0);
-	return (1);
+int     is_valid(char   *base)
+{
+        int     i;
+        int     j;
+
+        i = 0;
+        while (base[i])
+        {
+                if (base[i] == '+' || base[i] == '-')
+                        return (0);
+                if(base[i] == ' ' || (base[i] >= 9 && base[i] <= 13))
+                        return (0);
+                j = i + 1;
+                while (base[j] != '\0')
+                {
+                        if (base[i] == base[j])
+                                return (0);
+                        j++;
+                }
+                i++;
+        }
+        if (i <= 1)
+                return (0);
+        return (1);
 }
 
-int	in_string(char a, char	*src)
+int     in_string(char a, char  *src)
 {
-	int	i;
+        int     i;
 
-	i = 0;
-	while (src[i] != '\0')
-	{
-		if (src[i++] == a)
-			return (1);
-	}
-	return (0);
+        i = 0;
+        while (src[i] != '\0')
+        {
+                if (src[i++] == a)
+                        return (1);
+        }
+        return (0);
 }
 
-int	index_of(char a, char *src)
+int	index_of(char a, char *str)
 {
 	int	i;
 
 	i = 0;
-	while (src[i] != '\0')
+	while (str[i] != '\0')
 	{
-		if (src[i] == a)
-			return (i);
+		if(str[i] == a)
+			return i;
 		i++;
 	}
-	return (-1);
+	return -1;
 }
 
-int	to_decimal(char *str, int len, char *base)
+int     to_dicimal(char *str, int len, char *base)
 {
-	int	i;
-	int	re;
-	int	base_len;
-	int	pow;
-	int	j;
+        int	i;
+        long	re;
+        int	base_len;
+        int	pow;
+        int	j;
 
-	i = 0;
-	re = 0;
-	base_len = 0;
-	while (base[base_len] != '\0')
-		base_len++;
-	while (i < len)
-	{
-		pow = 1;
-		j = 1;
-		while (j++ < len - i)
-			pow *= base_len;
-		re += index_of(str[i], base) * pow;
-		i++;
-	}
-	return (re);
+        i = 0;
+        re = 0;
+        base_len = 0;
+        while (base[base_len] != '\0')
+                base_len++;
+        while (in_string(str[i], base))
+        {
+                pow = 1;
+                j = 1;
+                while (j++ < len - i)
+                        pow *= base_len;
+                re += index_of(str[i], base) * pow;
+                i++;
+        }
+        return (re);
 }
 
 int	ft_atoi_base(char *str, char *base)
 {
+	long	nb;
 	int	i;
+	int	len;
 	int	sign;
-	int	nb;
-	int	start;
-
-	sign = 1;
+	
 	i = 0;
 	nb = 0;
-	start = 0;
+	sign = 1;
+	len = 0;
 	if (is_valid(base))
 	{
-		while (str[i] != '\0')
+		while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
+			i++;
+		while (str[i] == '-' || str[i] == '+')
 		{
-			if((str[i] == '-' || str[i] == '+') && start == 1)
-					return (0);
-			if(str[i] == '-')
-				sign =* -1;
-			else if (in_string(str[i], base))
-			{
-				while (in_string(str[i++], base))
-					nb++;
-				return (to_decimal(&str[i - nb - 1], nb, base) * sign);
-			}
+			if (str[i] == '-')
+				sign *= -1;
 			i++;
 		}
+		if (in_string(str[i], base))
+		{
+			while(in_string(str[i + len], base))
+				len++;
+			nb = to_dicimal(str + i, len, base);
+		}
 	}
-	return (0);
+	return (nb * sign);
 }
