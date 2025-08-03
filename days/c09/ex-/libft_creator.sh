65ad151b4389ea8304b87681106fc5d794252309
Achraf Merkht <amerkht@e1r13p9.1337.ma>
2025-08-03 11:44:54 +0100

    merge

--- days/c09/ex-/libft_creator.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cc -c ft_putchar.c -o ft_putchar.o
-cc -c ft_putstr.c -o ft_putstr.o
-cc -c ft_strcmp.c -o ft_strcmp.o
-cc -c ft_strlen.c -o ft_strlen.o
-cc -c ft_swap.c -o ft_swap.o
-
-ar rcs libft.a ft_putstr.o ft_strcmp.o ft_strlen.o ft_swap.o
