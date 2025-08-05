2902332d002a98ab26c87c4a9c6b97b51307708f
Achraf Merkht <amerkht@e2r10p12.1337.ma>
2025-08-05 10:17:43 +0100

    ok

--- days/c08/ex04/ft_strs_to_tab.c
+++ days/c08/ex04/ft_strs_to_tab.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strs_to_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amerkht <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/05 10:12:04 by amerkht           #+#    #+#             */
+/*   Updated: 2025/08/05 10:17:26 by amerkht          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 #include "ft_stock_str.h"
 
@@ -13,41 +25,56 @@ int	ft_strlen(char *str)
 
 char	*ft_strdup(char *src)
 {
-	char	*copy;
+	char	*dest;
 	int		i;
 
 	i = 0;
 	while (src[i])
 		i++;
-	copy = malloc((sizeof(char) * i) + 1);
-	if (!copy)
+	dest = malloc((sizeof(char) * i) + 1);
+	if (!dest)
 		return (NULL);
 	i = 0;
 	while (src[i])
 	{
-		copy[i] = src[i];
+		dest[i] = src[i];
 		i++;
 	}
-	copy[i] = '\0';
-	return (copy);
+	dest[i] = '\0';
+	return (dest);
+}
+
+void	free_arr(t_stock_str *arr, int index)
+{
+	int	i;
+
+	i = 0;
+	while (i <= index)
+		free(arr[i++].copy);
+	free(arr);
 }
 
 struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
 {
-	t_stock_str	*output;
+	t_stock_str	*arr;
 	int			i;
 
 	i = 0;
-	output = malloc(sizeof(t_stock_str) * (ac + 1));
-	if (!output)
+	arr = malloc(sizeof(t_stock_str) * (ac + 1));
+	if (!arr)
 		return (NULL);
 	while (i < ac)
 	{
-		output[i].size = ft_strlen(av[i]);
-		output[i].str = av[i];
-		output[i].copy = ft_strdup(av[i]);
+		arr[i].size = ft_strlen(av[i]);
+		arr[i].str = av[i];
+		arr[i].copy = ft_strdup(av[i]);
+		if (!arr[i].copy)
+		{
+			free_arr(arr, i);
+			return (NULL);
+		}
 		i++;
 	}
-	output[i].str = 0;
-	return (output);
+	arr[i].str = 0;
+	return (arr);
 }
