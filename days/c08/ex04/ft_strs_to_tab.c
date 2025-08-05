f96edb2d96c814cb72c9fb7b84052030bda0d616
NIGANE <merkhtachraf163@gmail.com>
2025-08-05 10:04:02 +0100

    correct c08

--- days/c08/ex04/ft_strs_to_tab.c
+++ days/c08/ex04/ft_strs_to_tab.c
@@ -1,72 +1,53 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strs_to_tab.c                                   :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: amerkht <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/01 22:36:33 by amerkht           #+#    #+#             */
-/*   Updated: 2025/08/04 11:22:53 by amerkht          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "ft_stock_str.h"
 #include <stdlib.h>
+#include "ft_stock_str.h"
 
 int	ft_strlen(char *str)
 {
 	int	i;
 
-	if (!str)
-		return (0);
 	i = 0;
 	while (str[i])
 		i++;
 	return (i);
 }
 
-char	*ft_strdup(char *str, int size)
+char	*ft_strdup(char *src)
 {
+	char	*copy;
 	int		i;
-	char	*dest;
 
-	if (!str)
-		return (NULL);
-	dest = malloc(sizeof(char) * (size + 1));
-	if (!dest)
+	i = 0;
+	while (src[i])
+		i++;
+	copy = malloc((sizeof(char) * i) + 1);
+	if (!copy)
 		return (NULL);
 	i = 0;
-	while (str[i] && i < size)
+	while (src[i])
 	{
-		dest[i] = str[i];
+		copy[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
-	return (dest);
+	copy[i] = '\0';
+	return (copy);
 }
 
 struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
 {
-	t_stock_str	*linked;
+	t_stock_str	*output;
 	int			i;
 
-	linked = NULL;
-	if (!av || ac <= 0)
-		return (NULL);
-	linked = malloc(sizeof(t_stock_str) * (ac + 1));
-	if (!linked)
-		return (NULL);
-	linked[ac].str = 0;
 	i = 0;
+	output = malloc(sizeof(t_stock_str) * (ac + 1));
+	if (!output)
+		return (NULL);
 	while (i < ac)
 	{
-		linked[i].size = ft_strlen(av[i]);
-		linked[i].str = av[i];
-		linked[i].copy = ft_strdup(av[i], linked[i].size);
+		output[i].size = ft_strlen(av[i]);
+		output[i].str = av[i];
+		output[i].copy = ft_strdup(av[i]);
 		i++;
 	}
-	linked[i].str = 0;
-	linked[i].size = 0;
-	linked[i].copy = 0;
-	return (linked);
+	output[i].str = 0;
+	return (output);
 }
