31ed99824683e820d960aa8365575bbebe3be717
Achraf Merkht <amerkht@e2r10p12.1337.ma>
2025-08-05 09:59:30 +0100

    finish c09

--- days/c09/ex00/libft_creator.sh
+++ days/c09/ex00/libft_creator.sh
@@ -6,15 +6,15 @@
 #    By: amerkht <marvin@42.fr>                     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/08/04 10:41:10 by amerkht           #+#    #+#              #
-#    Updated: 2025/08/04 10:41:13 by amerkht          ###   ########.fr        #
+#    Updated: 2025/08/04 15:05:32 by amerkht          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/bash
-cc -c ft_putchar.c -o ft_putchar.o
-cc -c ft_putstr.c -o ft_putstr.o
-cc -c ft_strcmp.c -o ft_strcmp.o
-cc -c ft_strlen.c -o ft_strlen.o
-cc -c ft_swap.c -o ft_swap.o
+cc -Wall -Wextra -Werror -c ft_putchar.c -o ft_putchar.o
+cc -Wall -Wextra -Werror -c ft_putstr.c -o ft_putstr.o
+cc -Wall -Wextra -Werror -c ft_strcmp.c -o ft_strcmp.o
+cc -Wall -Wextra -Werror -c ft_strlen.c -o ft_strlen.o
+cc -Wall -Wextra -Werror -c ft_swap.c -o ft_swap.o
 
-ar rcs libft.a ft_putstr.o ft_strcmp.o ft_strlen.o ft_swap.o
+ar rcs libft.a ft_putchar.o ft_putstr.o ft_strcmp.o ft_strlen.o ft_swap.o
